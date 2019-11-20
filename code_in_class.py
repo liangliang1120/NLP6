ff96419283aa53abbd3fd9c2d061bed15917341d
liangliang1120 <35073431+liangliang1120@users.noreply.github.com>
2019-11-20 18:22:31 +0800

    Add files via upload

---
+++ code_in_class.py
@@ -0,0 +1,147 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import os
+try:
+  # %tensorflow_version only exists in Colab.
+%tensorflow_version 2.x  #gpu
+except Exception:
+  pass
+import tensorflow as tf
+
+import cProfile
+tf.executing_eagerly()
+
+x = [[2.]]
+m = tf.matmul(x,x)
+print("x matmul x = {}".format(m))
+
+a = tf.constant([[1,2],
+                 [3,4]])
+print(a)
+
+#Broadcasting
+b = tf.add(a,1)
+print(b)
+
+
+#element-wise multiplication
+print(a*b)
+
+
+print(tf.matmul(a,b))
+
+import numpy as np
+
+c = np.multiply(a,b)
+print(c)
+
+
+#Transfer a tensor to numpy array
+print(a.numpy())
+
+'''
+Computing gradients
+Automatic differentiation is useful for implementing machine learning algorithms such as backpropagation for training neural networks. During eager execution, use tf.GradientTape to trace operations for computing gradients later
+'''
+
+w = tf.Variable([[1.0]])
+with tf.GradientTape() as tape:
+    loss = w * w
+grad = tape.gradient(loss,w)
+print(grad)
+
+(x_train,y_train),(x_test,y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train[:10000,:,:]
+y_train = y_train[:10000]
+x_test = x_test[:1000,:,:]
+y_test = y_test[:1000]
+
+
+x_train = tf.cast(x_train[...,tf.newaxis]/255, tf.float32),
+x_test = tf.cast(x_test[...,tf.newaxis]/255, tf.float32),
+
+#y_train = y_train.astype('float32')
+#y_test = y_test.astype('float32')
+y_train = tf.keras.utils.to_categorical(y_train,10)
+y_test = tf.keras.utils.to_categorical(y_test,10)
+
+#Build the model using Sequential
+
+mnist_model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32,[3,3],activation='relu',
+                          input_shape=(28,28,1)),
+    tf.keras.layers.Conv2D(64,[3,3],activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128,activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(10,activation="softmax")
+])
+
+
+mnist_model.summary()
+
+#Build the model using Model
+inputs = tf.keras.Input(shape=(None,None,1),name="digits")
+conv_1 = tf.keras.layers.Conv2D(16,[3,3],activation="relu")(inputs)
+conv_2 = tf.keras.layers.Conv2D(16,[3,3],activation="relu")(conv_1)
+ave_pool = tf.keras.layers.GlobalAveragePooling2D()(conv_2)
+outputs = tf.keras.layers.Dense(10)(ave_pool)
+mnist_model_2 = tf.keras.Model(inputs=inputs,outputs=outputs)
+
+
+mnist_model_2.summary()
+
+'''
+Two training methods
+Use keras fit method
+'''
+mnist_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=1e-3),
+                    validation_split=0.1,shuffle=True,
+                   loss = tf.keras.losses.categorical_crossentropy,
+                   metrics = ["accuracy"])
+
+mnist_model_2.compile(loss = tf.keras.losses.categorical_crossentropy,
+                      optimizer=tf.keras.optimizers.Adam(),
+                     metrics=["accuracy"])
+
+mnist_model.fit(x_train,y_train,batch_size=128,epochs=3)
+
+
+mnist_model.evaluate(x_test,y_test)
+
+x_train[0]
+
+mnist_model.predict(x_test[0][1])
+
+'''
+Use TF 2.0
+'''
+
+(x_train,y_train),(x_test,y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train[:10000,:,:]
+y_train = y_train[:10000]
+x_test = x_test[:1000,:,:]
+y_test = y_test[:1000]
+
+dataset = tf.data.Dataset.from_tensor_slices(
+(tf.cast(x_train[...,tf.newaxis]/255, tf.float32),
+ tf.cast(y_train,tf.int64)))
+dataset = dataset.shuffle(1000).batch(32)
+
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_history = []
+
+for epoch in range(5):
+
+    for (batch, (images, labels)) in enumerate(dataset):
+        with tf.GradientTape() as tape:
+            logits = mnist_model(images, training=True)
+            loss_value = loss(labels, logits)
+
+        grads = tape.gradient(loss_value, mnist_model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, mnist_model.trainable_variables))
+
+    print("Epoch {} finishted".format(epoch))
+
